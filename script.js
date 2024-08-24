2885df6ac590d93976851706a6ee32ed0ba86fda
danendral <dlohanata@gmail.com>
2024-08-24 13:38:17 +0800

    Wrap grid creation into a function

--- script.js
+++ script.js
@@ -6,22 +6,36 @@ container.style.height = "960px";
 container.style.display = "flex";
 container.style.flexDirection = "column";
 
-// Create a 16x16 grid
-for (let i=0; i<16; i++) {
-    const containerRow = document.createElement("div");
-
-    containerRow.style.display = "flex";
-    containerRow.style.flex = "1 1 100%";
+// Function to create grid based on the size
+function createGrid(gridSize) {
+    for (let i=0; i<gridSize; i++) {
+        const containerRow = document.createElement("div");
     
-    for (let j=0; j<16; j++) {
-        const gridItem = document.createElement("div");
-        gridItem.textContent = "grid";
-
-        gridItem.style.width = 60;
-        gridItem.style.flex = "1 1 100%";
-
-        containerRow.appendChild(gridItem);
-    }
+        containerRow.style.display = "flex";
+        containerRow.style.flex = "1 1 100%";
+        
+        for (let j=0; j<gridSize; j++) {
+            const gridItem = document.createElement("div");
+            gridItem.style.flex = "1 1 100%";
+            gridItem.style.border = "solid 1px"
+            gridItem.addEventListener("mouseover", () => {
+                gridItem.style.backgroundColor = "grey";
+            })    
+            containerRow.appendChild(gridItem);
+        }
     
-    container.appendChild(containerRow);
+        container.appendChild(containerRow);
+    }    
+
 }
+createGrid(16);
+
+// const resizeButton = document.querySelector("#resizeButton");
+
+// console.log(resizeButton);
+
+// function resizeGrid(gridSize) {
+//     console.log("TEST", gridSize);
+// };
+
+// resizeButton.addEventListener("click", console.log("AA"));
